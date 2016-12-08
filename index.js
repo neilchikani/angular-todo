a54e976982c97116014e0005330c08ab38880c78
neilchikani <chikaninilay@gmail.com>
2016-12-08 13:00:43 +0530

    some changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express'),
 	app = express(),
 	path = require('path');
 
-app.use(express.static('public'));
+app.use(express.static('public') );
 app.listen(3000, function(){
 	console.log("Hello server is listening on 3000");
 });
